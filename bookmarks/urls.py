8c6590022b72d1a240c9709100a70ec69f82ac72
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-03-23 20:14:54 +0530

    Sharing Content on Your Website

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -14,17 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("account/", include("account.urls")),
-    path("social-auth/", include("social_django.urls", namespace="social")),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path(
+        'social-auth/',
+        include('social_django.urls', namespace='social'),
+    ),
+    path('images/', include('images.urls', namespace='images')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
